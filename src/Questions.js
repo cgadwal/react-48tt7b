a9122c5e1ac3928e3d8a04b675a46155dd5b3021
cgadwal <chitragadwal@gmail.com>
2020-09-05 14:41:29 -0400

    2 files modified

--- src/Questions.js
+++ src/Questions.js
@@ -4,27 +4,29 @@ import { BallResizer } from "./Slider";
 export class Questions extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: 0};
+    this.state = {pageNumb: 0};
 
     // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-    this.handleClick2 = this.handleClick2.bind(this);
+    this.nextButtonClick = this.nextButtonClick.bind(this);
+    this.prevButtonClick = this.prevButtonClick.bind(this);
   }
 
-  handleClick() {
+  nextButtonClick() {
     this.setState((state) => {
-      return {isToggleOn: state.isToggleOn + 1}
+      return {pageNumb: state.pageNumb + 1}
     });
+
   }
   
-  handleClick2() {
+  prevButtonClick() {
     this.setState((state) => {
-      return {isToggleOn: state.isToggleOn - 1}
+      return {pageNumb: state.pageNumb - 1}
     });
   }
 
-  submit() {
 
+  submit() {
+    //to do: implement
   }
 
   render() {
@@ -43,24 +45,26 @@ export class Questions extends React.Component {
             <div className="bar">
               {question.questionTitle}
             </div>
-            <input type="radio" id={question.answer1} name={question[0]} value={question.answer1} />
+            <div>
+            <input type="radio" id={question.answer1} name={question.questionTitle} value={question.answer1} />
             <label htmlFor={question.answer1}>{question.answer1} </label>
             <br />
             
-            <input type="radio" id={question.answer2} name={question[0]} value={question.answer2} />
+            <input type="radio" id={question.answer2} name={question.questionTitle} value={question.answer2} />
             <label htmlFor={question.answer2}>{question.answer2} </label>
             <br />
 
-            <input type="radio" id={question.answer3} name={question[0]} value={question.answer3} />
+            <input type="radio" id={question.answer3} name={question.questionTitle} value={question.answer3} />
             <label htmlFor={question.answer3}>{question.answer3} </label>
             <br />
 
-            <input type="radio" id={question.answer4} name={question[0]} value={question.answer4} />
+            <input type="radio" id={question.answer4} name={question.questionTitle} value={question.answer4} />
             <label htmlFor={question.answer4}>{question.answer4} </label>
+            </div>
             <br /><br />
             Level of certainity: 
             <div className="ballSlider">
-              <BallResizer name={question[0]} initialSize={150} minSize={150} maxSize={250} />
+              <BallResizer name={question.questionTitle} initialSize={150} minSize={150} maxSize={250} />
             </div>
        </div>   
        
@@ -69,27 +73,28 @@ export class Questions extends React.Component {
 
     return ( 
       <div>
-            <nav>{questions[this.state.isToggleOn]}</nav> 
+            <nav>{questions[this.state.pageNumb]}</nav> 
             <br />
             <div class="buttons">
-              { this.state.isToggleOn > 0 ?  this.prevButton() : '' } 
-              
-              Page: {this.state.isToggleOn + 1}
-                
-              {this.state.isToggleOn < 5 ? this.nextButton() : this.submitButton()}
+              { this.state.pageNumb > 0 ?  this.prevButton() : '' } 
+              &nbsp;
+              Page: {this.state.pageNumb + 1}
+              &nbsp;
+              {this.state.pageNumb < 5 ? this.nextButton() : this.submitButton()}
             </div>
       </div> 
     );
   }
   
   prevButton() {
-      return (<button onClick={this.handleClick2}>
-        Prev
+      return (<button class="previous round" onClick={this.prevButtonClick}>
+        &#8249;
       </button>); 
+
   }
   nextButton() {
-      return (<button onClick={this.handleClick}>
-        Next
+      return (<button class="next round" onClick={this.nextButtonClick} >
+        &#8250;
       </button>); 
   }
   submitButton() {
